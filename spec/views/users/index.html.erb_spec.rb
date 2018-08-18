2fbcdfb3af318f544cfa71b447af2f083ef18559
Luc Dang <luc@tinypulse.com>
2018-08-18 14:09:18 +0700

    add some spec

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -3,12 +3,20 @@ require 'rails_helper'
 RSpec.describe "users/index", type: :view do
   before(:each) do
     assign(:users, [
-      User.create!(),
-      User.create!()
+      User.create!(
+        :email => "Email",
+        :id => 2
+      ),
+      User.create!(
+        :email => "Email",
+        :id => 2
+      )
     ])
   end
 
   it "renders a list of users" do
     render
+    assert_select "tr>td", :text => "Email".to_s, :count => 2
+    assert_select "tr>td", :text => 2.to_s, :count => 2
   end
 end
