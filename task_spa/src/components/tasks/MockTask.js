bda01e70c7108b8081d427faea71505bdbaf5346
Luc Dang <luc@tinypulse.com>
2018-08-18 14:08:47 +0700

    html for assume user

---
+++ task_spa/src/components/tasks/MockTask.js
@@ -0,0 +1,16 @@
+const mockTasks = [
+  {
+    id: 1,
+    title: 'Learn Redux',
+    description: 'The store actions and reducers omg',
+    status: 'In Progress',
+  },
+  {
+    id: 2,
+    title: 'Peace on Earth',
+    description: 'No big deal.',
+    status: 'In Progress'
+  }
+];
+
+export default mockTasks;
