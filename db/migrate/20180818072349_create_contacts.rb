bf97eb0b7f165c7747767ad4d6541963c1834a41
Luc Dang <luc@tinypulse.com>
2018-08-19 11:03:17 +0700

    change structure data, revoew created_at

--- db/migrate/20180818072349_create_contacts.rb
+++ db/migrate/20180818072349_create_contacts.rb
@@ -1,11 +1,9 @@
 class CreateContacts < ActiveRecord::Migration[5.1]
   def change
     create_table :contacts do |t|
+      t.string :phone_number, limit: 20
       t.date :activation_date
       t.date :deactivation_date
-      t.string :phone_number, limit: 20
-
-      t.timestamps
     end
   end
 end
